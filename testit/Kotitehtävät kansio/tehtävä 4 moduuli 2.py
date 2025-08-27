a4b8c3e38b13209a91f88ce4744ff602f869b84e
Carl-MET <carl.inkeroinen@metropolia.fi>
2025-08-27 21:12:51 +0300

    tehtävä 4

---
+++ testit/Kotitehtävät kansio/tehtävä 4 moduuli 2.py
@@ -0,0 +1,7 @@
+
+yksi = float(input("Anna jokin luku"))
+kaksi = float(input("Anna jokin luku"))
+kolme = float(input("Anna jokin luku"))
+print("Lukujen summa on:",str((yksi+kaksi+kolme)))
+print("Lukujen tulo on:",str((yksi*kaksi*kolme)))
+print("lukujen keskiarvo on:",str((yksi+kaksi+kolme)/3))
