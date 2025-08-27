9683bf71aa2aebae158602ebe026eed1fc28fdbf
Carl-MET <carl.inkeroinen@metropolia.fi>
2025-08-27 20:52:26 +0300

    Ohjelma 1

---
+++ testit/Kotitehtävät kansio/tehtävä 3 moduuli 2.py
@@ -0,0 +1,4 @@
+kanta = float(input("anna suorakulmion kanta"))
+korkeus = float(input("anna suorakulmion korkeus"))
+print("Suorakulmion pinta-ala on:" + str(kanta*korkeus))
+print("Suorakulmion piiri on:" + str((kanta*2)+korkeus*2))
