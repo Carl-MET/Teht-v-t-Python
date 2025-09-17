77d62a8883bdf1a0a364954223253941d7de5976
CARL-MET <carl.inkeroinen@metropolia.fi>
2025-09-17 16:19:26 +0300

    tehtävä 2

---
+++ testit/Kotitehtävät kansio/moduuli 4 tehtävä 2.py
@@ -0,0 +1,8 @@
+maara = float(input('anna tuuma määrä'))
+tuuma = 2.54
+while maara > 0:
+    if maara>0:
+        tuumien_summa=tuuma+maara
+        (print(f'tuumat senteissä:{tuumien_summa}' ))
+    maara = float(input('anna tuuma määrä' ))
+print('ohjelma lopetti toimintansa.')
