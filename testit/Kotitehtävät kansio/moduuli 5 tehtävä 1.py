a9ae643ce5cb3e135c02d9220accc463f9b9fead
CARL-MET <carl.inkeroinen@metropolia.fi>
2025-10-01 21:03:47 +0300

    tehtävä 1

---
+++ testit/Kotitehtävät kansio/moduuli 5 tehtävä 1.py
@@ -0,0 +1,22 @@
+import random
+from random import randrange
+
+noppa= int(input('anna arpakuutioden määrä'))
+kuutiot=[0]*noppa
+for heitot in kuutiot:
+    print(heitot,randrange(1,7))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
