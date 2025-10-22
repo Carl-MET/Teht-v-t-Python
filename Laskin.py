28b86ca9dc4f03f84bb66bc2674a904974d5cfd6
CARL-MET <carl.inkeroinen@metropolia.fi>
2025-10-22 20:00:07 +0300

    tehtävä 2

--- Laskin.py
+++ Laskin.py
@@ -1,18 +1,23 @@
-print("valitse mitä toimintoa haluat käyttää\n A on yhteislasku \nB on vähennyslasku"
-      " \nC on kertolasku \nD on jakolasku")
-valinta = input("valintasi A-D:").upper()
+while True:
+    print("valitse mitä toimintoa haluat käyttää\n A on yhteislasku \nB on vähennyslasku"
+          " \nC on kertolasku \nD on jakolasku")
+    valinta = input("valintasi A-D: Q lopettaa ohjelman").upper()
 
-a = float(input("Anna ensimmäinen luku:"))
-b = float(input("Anna toinen luku:"))
-if valinta == "A":
-    print("yhteenlasku",a+b)
-elif valinta == "B":
-    print("vähennyslasku",a-b)
-elif valinta == "C":
-    print("kertolasku",a*b)
-elif valinta == "D":
-    print("jakolasku",a/b)
-else:
-    print("valintasi on virheellinen")
+    if valinta == 'Q':
+        print('ohjelma lopetetaan, kiitos hei')
+        break
+
+    a = float(input("Anna ensimmäinen luku:"))
+    b = float(input("Anna toinen luku:"))
+    if valinta == "A":
+        print("yhteenlasku",a+b)
+    elif valinta == "B":
+        print("vähennyslasku",a-b)
+    elif valinta == "C":
+        print("kertolasku",a*b)
+    elif valinta == "D":
+        print("jakolasku",a/b)
+    else:
+        print("valintasi on virheellinen")
 
 
