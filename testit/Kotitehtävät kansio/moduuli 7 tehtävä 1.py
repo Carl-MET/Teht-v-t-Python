8fa13fb626be6ed68b5bd8e46c70fe4092ac955f
CARL-MET <carl.inkeroinen@metropolia.fi>
2025-11-13 12:38:11 +0200

    tehtävä 1

--- testit/Kotitehtävät kansio/moduuli 7 tehtävä 1.py
+++ testit/Kotitehtävät kansio/moduuli 7 tehtävä 1.py
@@ -1,6 +1,6 @@
 vuodenajat = {    12: 'talvi',1: 'talvi',2: 'talvi',3: 'kevät',4: 'kevät',
-                  5: 'kevät',6: 'kesä',7: 'kesä',8: 'kesä',9: 'syksy',10: 'syksy',11: 'syksy'
-    }
+                  5: 'kevät',6: 'kesä',7: 'kesä',8: 'kesä',9: 'syksy',10: 'syksy',11: 'syksy'}
+
 vuodenaika =int(input('Minkä kuukauden vuodenajan haluat tarkastaa? anna kuukauden numero muodossa '
                       'esim tammikuu= 1'))
 
